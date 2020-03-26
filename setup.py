f7af5c9d58ca534c1f1d97263f2567a84920d04c
Kukushkin Alexander <alex@alexkuk.ru>
2020-03-26 08:35:33 +0300

    Setup meta

--- setup.py
+++ setup.py
@@ -2,8 +2,29 @@
 from setuptools import setup, find_packages
 
 
+with open('README.md') as file:
+    description = file.read()
+
+
 setup(
-    name='nerus',
+    name='naeval',
     version='0.1.0',
+
+    description='Comparing quality and performance of NLP systems for Russian language',
+    long_description=description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/natasha/naeval',
+    author='Alexander Kukushkin',
+    author_email='alex@alexkuk.ru',
+    license='MIT',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='nlp, russian, evaluation',
+
     packages=find_packages()
 )
